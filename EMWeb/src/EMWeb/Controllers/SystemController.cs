d257e9b7c49135b5fea5b0cdcabea677108c2f3d
Cream2015 <343224963@qq.com>
2016-04-21 13:59:39 +0800

    init log

--- EMWeb/src/EMWeb/Controllers/SystemController.cs
+++ EMWeb/src/EMWeb/Controllers/SystemController.cs
@@ -4,7 +4,10 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Mvc;
 using Microsoft.AspNet.Authorization;
+using Microsoft.Data.Entity;
 using EMWeb.Models;
+using EMWeb.ViewModels;
+
 
 namespace EMWeb.Controllers
 {
@@ -19,9 +22,51 @@ namespace EMWeb.Controllers
                 .Where(x => x.Title == college)
                 .Single();
             DB.Majors.Add(major);
+            var log = new Log
+            {
+                UserId = User.Current.Id,
+                Roles = Roles.系主任,
+                Operation = Operation.添加专业,
+                Time = DateTime.Now,
+                Number = major.Id,
+            };
+            DB.Logs.Add(log);
             major.CollegeId = cid.Id;
             DB.SaveChanges();
             return Content("success");
+        }
+        [Authorize(Roles=("系主任"))]
+        [HttpGet]
+        public IActionResult Log()
+        {
+            var log = DB.Logs
+                .OrderByDescending(x => x.Time)
+                .ToList();
+            //if (log.Count() != 0)
+            //{
+            //    var ret = new List<SystemLog>();
+            //    foreach (var x in log)
+            //    {
+            //        ret.Add(new SystemLog
+            //        {
+            //            Id = x.Id,
+            //            AdminNumber = DB.Teachers.Where(y => y.UserId == x.UserId).SingleOrDefault().Number,
+            //            AdminName = DB.Teachers.Where(y => y.UserId == x.UserId).SingleOrDefault().Name,
+            //            Role = x.Roles.ToString(),
+            //            Operation = x.Operation.ToString(),
+            //            Time = x.Time,
+            //            TargetNumber = x.Number,
+            //        });
+            //    };
+
+            //    return View(log);
+            //}
+            //else
+            //{
+            //    return RedirectToAction("Error", "Home");
+            //}
+            return View(log);
+            
         }
     }
 }
