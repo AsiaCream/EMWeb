7882218d070fd082153bea2f3265edfd22518972
Cream2015 <343224963@qq.com>
2016-04-22 22:34:49 +0800

    init viewmodels

---
+++ EMWeb/src/EMWeb/ViewModels/MySubjectLog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMWeb.ViewModels
+{
+    public class MySubjectLog
+    {
+        public int Id { get; set; }
+        public DateTime Time { get; set; }
+        public int SNumber { get; set; }
+        public string STitle { get; set; }
+        public string Teacher { get; set; }
+    }
+}
---
+++ EMWeb/src/EMWeb/ViewModels/SubjectLog.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMWeb.ViewModels
+{
+    public class SubjectLog
+    {
+        public int Id { get; set; }
+        public int StudentNumber { get; set; }
+        public string StudentName { get; set; }
+        public int SNumber { get; set; }
+        public string SName { get; set; }
+        public DateTime Time { get; set; }
+    }
+}
