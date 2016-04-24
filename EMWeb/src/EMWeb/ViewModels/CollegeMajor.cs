b0ba43dc22dda86b534f049be7527f9e1bac789c
Cream2015 <343224963@qq.com>
2016-04-24 22:54:34 +0800

    add major display

---
+++ EMWeb/src/EMWeb/ViewModels/CollegeMajor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMWeb.ViewModels
+{
+    public class CollegeMajor
+    {
+        public int CollegeNumber { get; set; }
+        public string CollegeName { get; set; }
+        public int MajorNumber { get; set; }
+        public string MajorName { get; set; }
+    }
+}
