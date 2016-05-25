495b9e6bc61792ff764e591cb5b1571fd3faaa44
Cream2015 <343224963@qq.com>
2016-05-25 22:11:33 +0800

    file→fileinfo

---
+++ EMWeb/src/EMWeb/Models/FileInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMWeb.Models
+{
+    public enum FType
+    {
+        论文,
+        报告,
+        源代码,
+        文档,
+    }
+    public class FileInfo
+    {
+        public int Id { get; set; }
+        public FType FType { get; set; }
+        public string Title { get; set; }
+        public string Path { get; set; }
+        public DateTime CreateTime { get; set; }
+
+        [ForeignKey("Student")]
+        public int StudentId { get; set; }
+        public virtual Student Student { get; set; }
+    }
+}
