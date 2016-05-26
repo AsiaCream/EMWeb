3afeb6d4f1292b2f036f4c9ceac1cba8d9586b4d
Cream2015 <343224963@qq.com>
2016-05-26 20:16:07 +0800

    add viewmodels studentlist

---
+++ EMWeb/src/EMWeb/ViewModels/StudentList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EMWeb.ViewModels
+{
+    public class StudentList
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string StudentNumber { get; set; }
+        public string College { get; set; }
+        public string Major { get; set; }
+        public string CreateTime { get; set; }
+    }
+}
