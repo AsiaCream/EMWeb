f37b547a8c1a397a92d64d9df5b29bf2e7356de0
Cream2015 <343224963@qq.com>
2016-06-05 22:53:15 +0800

    User ADD avatar pro

--- EMWeb/src/EMWeb/Models/User.cs
+++ EMWeb/src/EMWeb/Models/User.cs
@@ -4,10 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNet.Identity.EntityFramework;
-
+using System.ComponentModel.DataAnnotations.Schema;
+using CodeComb.AspNet.Upload.Models;
 namespace EMWeb.Models
 {
     public class User:IdentityUser<long>
     {
+        [ForeignKey("Avatar")]
+        public Guid? AvatarId { get; set; }
+        public virtual File Avatar { get; set; }
     }
 }
